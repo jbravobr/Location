51f119fc8b0304772952c62c678a24b188f9ca4f
Paulo Eduardo Correia <pauloeduardocorreia@Mac-mini-de-Paulo.local>
2016-12-23 13:56:42 -0200

    Add location

--- APP/Location.APP/Location.APP/Views/MainPage.xaml.cs
+++ APP/Location.APP/Location.APP/Views/MainPage.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Xamarin.Forms;
+using Plugin.Geolocator;
+using Xamarin.Forms.GoogleMaps;
+using System.Threading.Tasks;
 
 namespace Location.APP.Views
 {
@@ -6,15 +9,36 @@ namespace Location.APP.Views
     {
         public MainPage()
         {
-            //var bar = new Syncfusion.SfBarcode.XForms.SfBarcode();
-            //bar.Text = "teste";
-            //bar.Symbology = Syncfusion.SfBarcode.XForms.BarcodeSymbolType.QRCode;
-            /////this.layout = new StackLayout();
-            ///// this.Content.cc
-            //this.barcode = bar;
-
-
+			Informacoes().ConfigureAwait(false);
             InitializeComponent();
         }
+
+		protected async Task Informacoes()
+		{
+			try
+			{
+
+				var locator = CrossGeolocator.Current;
+				var position = await locator.GetPositionAsync(5000);
+
+				var actualPosition = new Position(position.Latitude, position.Longitude);
+
+				map.MoveToRegion(MapSpan.FromCenterAndRadius(
+					actualPosition , Distance.FromMiles(1)));
+				var pin = new Pin() 
+				{
+					Position = actualPosition,
+					Label = "",
+					Type = PinType.Generic,
+
+				};
+
+				map.Pins.Add(pin);
+			}
+			catch (System.Exception ex)
+			{
+				throw ex;
+			}
+		}
     }
 }
